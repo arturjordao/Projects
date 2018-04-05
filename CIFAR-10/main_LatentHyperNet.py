1-Bug FIX in LHN (Keras) 2- Added FLOPS computation

CIFAR-10/main_LatentHyperNet.py
@@ -18,22 +18,26 @@ if __name__ == '__main__':
     np.random.seed(12227)
 
     debug = True
-    layers = [44, 51]
-    n_comp = 19
+    layers = [45, 52, 55]
+    n_comp = 8
     dm_method = 'pls'
 
     X_train, y_train, X_test, y_test = func.cifar_vgg_data(debug)
 
 
     cnn_model = func.load_model(architecture_file='../architectures/cifar10VGG',
-                                weights_file='')
+                                weights_file='../weights/cifar10VGG++')
 
+    id_layer = ''
+    for i in range(0, len(cnn_model.layers)):
+        id_layer += '['+str(i)+' '+ str(type(cnn_model.get_layer(index=i))).split('.')[-1].replace('>', '') + '] '
+    print(id_layer)
     print('Layers{}'.format(layers)) if dm_method=='' else print('Layers{} Number of Components[{}] Method[{}]'.format(layers, n_comp, dm_method))
 
     if dm_method != '':
         hyper_net = LatentHyperNet(n_comp=n_comp, model=cnn_model, layers=layers, dm_method=dm_method)
     else:
-        hyper_net = LatentHyperNet(model=model, layers=layers)
+        hyper_net = LatentHyperNet(model=cnn_model, layers=layers)
 
     if hyper_net.dm_method is not None:
         hyper_net.fit(X_train, y_train)

CIFAR-10/predict_offline.py
@@ -23,8 +23,9 @@ def baseline(architecture_file, weights_file, X_train, X_test, y_train, y_test):
     cat_acc = recall_score(y_test, y_pred, average='macro')
 
     n_filters = func.count_filters(model)
+    flops, _ = func.compute_flops(model)
 
-    print('Number of Parameters [{}] Number of Filters [{}] Accuracy [{:.4f}] Categorical Accuracy [{:.4f}] Time[{:.4f}]'.format(model.count_params(), n_filters, acc, cat_acc, end - start))
+    print('Number of Parameters [{}] Number of Filters [{}] FLOPS [{}] Accuracy [{:.4f}] Time[{:.4f}]'.format(model.count_params(), n_filters, flops, acc, end - start))
 
 if __name__ == '__main__':
     np.random.seed(12227)
@@ -38,7 +39,7 @@ if __name__ == '__main__':
     initial_iteration = 0
     for i in range(initial_iteration, max_iterations):
 
-        file_name='models/model_iteration{}'.format(i)
+        file_name='../VIPNet/<>/model_iteration{}'.format(i)
         model = func.load_model(file_name, file_name)
 
         start = time.time()
@@ -47,8 +48,10 @@ if __name__ == '__main__':
 
         y_pred = np.argmax(y_pred, axis=1)
         acc = accuracy_score(y_test, y_pred)
-        cat_acc = recall_score(y_test, y_pred, average='macro')
+        #cat_acc = recall_score(y_test, y_pred, average='macro')
 
         n_filters = func.count_filters(model.get_layer(index=1))
-
-        print('Number of Parameters [{}] Number of Filters [{}] Accuracy [{:.4f}] Categorical Accuracy [{:.4f}] Time[{:.4f}]'.format(model.count_params(), n_filters, acc, cat_acc, end - start))
+        filter_layer = func.count_filters_layer(model.get_layer(index=1))
+        flops, _ = func.compute_flops(model.get_layer(index=1))
+        print('Number of Parameters [{}] Number of Filters [{}] FLOPS [{}] Accuracy [{:.4f}] Time[{:.4f}]'.format(model.count_params(), n_filters, flops, acc, end - start))
+        print('{}'.format(filter_layer))